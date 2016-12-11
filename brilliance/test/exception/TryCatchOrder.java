53553472bd1fe08d20e1d315f226dec01ef7b1da
Russell <jxoucjj@126.com>
2016-12-11 11:26:16 +0800

    try-catch-finally中直接return的执行顺序

--- brilliance/test/exception/TryCatchOrder.java
+++ brilliance/test/exception/TryCatchOrder.java
@@ -6,17 +6,29 @@ package exception;
 * @Description: try catch执行顺序
 * @author Russell Xun Jiang
 * @date 2016年12月10日 下午4:47:26
+* 0.有try，没有catch时，发生异常也会被捕捉，程序将默认抛出真正发生的异常。
+* 1.try或者catch中直接返回时，先返回给调用方，再执行finally中语句，所以finally中逻辑不影响调用方获取的结果，finally之后的代码不执行。
+* 2.finally中返回时（程序正常无异常抛出），按正常顺序执行。
 */
 public class TryCatchOrder {
 
 	public static void main(String[] args) {
-//		//只要try,没有catch
-//		int i = tryNoCatch();
-//		System.out.println("i="+i);
+		//只要try,没有catch
+		int i = tryNoCatch();
+		System.out.println("i="+i);
 		
-		//try catch执行顺序
-		int a = getInt2();
-		System.out.println("a="+a);
+		/**
+		 * try-catch-finally执行顺序
+		 */
+		//try中返回
+		int a1 = tryReturn();
+		System.out.println("a1="+a1);
+		//catch中返回
+		int a2 = catchReturn();
+		System.out.println("a2="+a2);
+		//finally中返回
+		int a3 = finallyReturn();
+		System.out.println("a3="+a3);
 		
 
 	}
@@ -31,7 +43,7 @@ public class TryCatchOrder {
 	public static int tryNoCatch(){
 		int i=0;
 		try{
-			i = 10;
+			i = 9;
 			i = i/0;
 		}finally{
 			
@@ -40,43 +52,79 @@ public class TryCatchOrder {
 	}
 	
 	/** 
-	* @Title: getInt1 
+	* @Title: tryReturn 
 	* @Description: try中return 
 	* @param @return    设定文件 
 	* @return int    返回类型 
 	* @throws 
+	* 在try中返回时，先结果返回给调用方，再执行finally中的代码，所以finally中的逻辑不会影响调用方获取的结果。
+	* 执行顺序：
+	* 	(1)->(2)返回给调用方，被调用方->(4)
 	*/ 
-	public static int getInt1(){
+	public static int tryReturn(){
+		System.out.println("tryReturn begin............");
 		int a = 0;
 		try{
-			a = 10;
-			return a;
+			a = 9;//(1)
+			return a;//(2)
 		}catch(Exception e){
-			
+			a = 10;//(3)
 		}finally{
-			a = 11;
+			a = 11;//(4)
 		}
+		System.out.println("tryReturn begin............");
+		return a;//(5)
+	}
+	
+	/** 
+	* @Title: catchReturn 
+	* @Description: catch中return 
+	* @param @return    设定文件 
+	* @return int    返回类型 
+	* @throws 
+	* 在catch中返回时，和try中返回一样，先将catch结果返回给调用方，再执行finally中的代码，所以finally中的逻辑不会影响调用方获取的结果。
+	* 执行顺序：
+	* 	(1)->(2)->(3)->(4)返回给调用方，被调用方->(5)
+	*/ 
+	public static int catchReturn(){
+		System.out.println("catchReturn begin............");
+		int a = 0;
+		try{
+			a = 9;//(1)
+			int b = 5/0;//(2)
+		}catch(Exception e){
+			a = 10;//(3)
+			return a;//(4)
+		}finally{
+			a = 11;//(5)
+		}
+		a = 12;//(6)
+		System.out.println("catchReturn end............");
 		return a;
 	}
 	
 	/** 
-	* @Title: getInt2 
+	* @Title: finallyReturn 
 	* @Description: finally中return
 	* @param @return    设定文件 
 	* @return int    返回类型 
 	* @throws 
+	* finally中返回，则严格按照顺序依次执行。
+	* 执行顺序：
+	* 	(1)->(3)->(4)返回给调用方
 	*/ 
 	@SuppressWarnings("finally")
-	public static int getInt2(){
+	public static int finallyReturn(){
+		System.out.println("finallyReturn begin............");
 		int a = 0;
 		try{
-			a = 10;
-			
+			a = 9;//(1)
 		}catch(Exception e){
-			
+			a = 10;//(2)
 		}finally{
-			a = 11;
-			return a;
+			a = 11;//(3)
+			System.out.println("finallyReturn end............");
+			return a;//(4)
 		}
 	}
 
