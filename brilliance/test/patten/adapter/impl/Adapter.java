32604c91fa40b43883d787c90559d9b1f212ff75
jiangxun <jxoucjj@126.com>
2022-04-06 08:17:11 +0800

    加入注释原因：适配器模式之简单工厂去创建各种适配器，无需工厂方法或抽象工厂。

--- brilliance/test/patten/adapter/impl/Adapter.java
+++ brilliance/test/patten/adapter/impl/Adapter.java
@@ -11,8 +11,12 @@ public class Adapter implements TransferAdapter{
         return null;
     }
 
-    //根据不同input,返回/创建不同对象。这里用简单工厂模式实现，
-    // 后续可refactor改善成工厂方法模式或抽象工厂模式
+    /**
+     * 根据不同input,返回/创建不同对象。这里用简单工厂模式实现。缺点：每次增加新的适配器，要改这里代码。
+     * 如果这里改成工厂方法模式或抽象工厂模式，虽然每次增加新的适配器，不用要改这里代码。但还是要在一个地方
+     * 维护"各种电器->各种适配器"的映射关系，每次增加新适配器，还是要改映射关系的代码。
+     * 所以索新就在这里维护好了，用简单方法模式，不用工厂方法或抽象工厂模式。
+     */
     public Adapter getAdapter(Appliance app){
         if(app instanceof USBAppliance){
             return new USBAdapter();
--- brilliance/test/patten/adapter/impl/BiHoleAdapter.java
+++ brilliance/test/patten/adapter/impl/BiHoleAdapter.java
@@ -1,9 +1,7 @@
 package patten.adapter.impl;
 
-import patten.adapter.TransferAdapter;
 import patten.adapter.model.Appliance;
 import patten.adapter.model.Electric;
-import patten.adapter.model.USBAppliance;
 import patten.adapter.model.USBElectric;
 
 public class BiHoleAdapter extends Adapter{
