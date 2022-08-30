7c16595a0a568deab123d8614639e8dc17cd61d7
jiangxun <jxoucjj@126.com>
2022-08-30 12:13:56 +0800

    无重复最长子串

--- brilliance/test/algorithm/coding/LeetCode.java
+++ brilliance/test/algorithm/coding/LeetCode.java
@@ -7,7 +7,7 @@ package algorithm.coding;
 public class LeetCode {
     public static void main(String[] args) {
 
-        //找出数列中最大的差值
+        //(0)找出数列中最大的差值
         int[] arr = {1,3,8,16,7,50,0,20,100};
         int[] arr2 = {1,5,1,2 };
         int[] arr3 = {-6,5,-2,-5,10,4};
@@ -21,7 +21,7 @@ public class LeetCode {
          maxDelta:16
          */
 
-        //两数相加(链表)
+        //(2)两数相加(链表)
         ListNode node1 = new ListNode (2,new ListNode (4,new ListNode (3)));
         ListNode node2 = new ListNode (5,new ListNode (6,new ListNode (4)));
         addTwoNumbers (node1, node2);
@@ -31,12 +31,23 @@ public class LeetCode {
          */
 
 
+        //(3)无重复字符串的最长子串
+        lengthOfLongestSubstring("abcabcbb");
+        lengthOfLongestSubstring("pwwkew");
+        lengthOfLongestSubstring("xyabcabcbb");
+        /**
+         * output:
+         * maxSubLength:3,maxSubStr:abc
+         maxSubLength:4,maxSubStr:wke
+         maxSubLength:5,maxSubStr:xyabc
+         */
+
     }
 
 
 
     /**
-     * 找出数列中最大的差值。注：只能是后面减去前面。
+     * (0)找出数列中最大的差值。注：只能是后面减去前面。
      * from citi
      * 如1，5，1，2 ->5-1=4
      * -6,5,-2,-5,10,4->10-(6)=16
@@ -61,13 +72,15 @@ public class LeetCode {
         return maxDelta;
     }
 
-    /**两数相加(链表)
+    /**(2)两数相加(链表)
      * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
      请你将两个数相加，并以相同形式返回一个表示和的链表。
      你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
      * 输入：l1 = [2,4,3], l2 = [5,6,4]
      输出：[7,0,8]
      解释：342 + 465 = 807.
+
+     分析：逐个取下一个节点的值(有就直接取，没有就默认为0)相加。
      * @param l1
      * @param l2
      * @return
@@ -117,6 +130,53 @@ public class LeetCode {
         }
         System.out.println ("combined node:" + value);
         return backNode;
+    }
+
+    /**
+     * (3)无重复字符串的最长子串.即子串中
+     * 输入: s = "abcabcbb"
+     输出: 3
+     解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+     输入: s = "bbbbb"
+     输出: 1
+     解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+
+     输入: s = "pwwkew"
+     输出: 3
+     解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+          请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+     分析：从头到尾遍历每个字符，逐个放到OK子串中。如果当前字符不在串中，则加入串并串长度+1；
+     否则OK子串变成有重复字符后面所有字符。
+     例子：xyabcabcbb
+     一开始xyabc最长且长度5，比较到第二个a时，有重复，新的OK子串是bca.
+
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring(String s) {
+        String subStr = new String(s.substring (0,1));
+        int maxSubLength = 1;
+        //maxSubStr打印用。
+        String maxSubStr = "";
+        for(int i=1; i<s.length ();i++){
+            String item = s.substring(i,i+1);
+            if(!subStr.contains(item)){
+                subStr += item;
+                maxSubLength++;
+                maxSubStr = subStr;
+            }else{
+                //有重复。则截取既有子串中，新的子串作为OK子串。
+                //这里取已OK串中有重复字符的后面的全部。
+                subStr = subStr.substring(subStr.indexOf(item)+1)+item;
+                maxSubLength = Math.max(maxSubLength, subStr.length ());
+
+            }
+        }
+
+        System.out.println("maxSubLength:"+maxSubLength+",maxSubStr:"+maxSubStr);
+        return maxSubLength;
     }
 
 
